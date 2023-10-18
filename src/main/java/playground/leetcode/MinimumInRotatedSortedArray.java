2691adbb25dea7156e070e8deaaad8412cd475da
Albert Snow <ajsnow2012@gmail.com>
2023-10-18 16:47:32 -0500

    rotated minimum first draft

--- src/main/java/playground/leetcode/MinimumInRotatedSortedArray.java
+++ src/main/java/playground/leetcode/MinimumInRotatedSortedArray.java
@@ -1,7 +1,30 @@
 package playground.leetcode;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+
 public class MinimumInRotatedSortedArray {
-    public static Integer findMinimumInRotatedSortedArrayTest(String right) {
-        return -1;
+    @Contract(pure = true)
+    public static Integer
+    findMinimumInRotatedSortedArrayTest(@NotNull List<Integer> rotatedSortedList) {
+        var leftIndex = 0;
+        var rightIndex = rotatedSortedList.size() - 1;
+        while(leftIndex < rightIndex) {
+            var leftValue = rotatedSortedList.get(leftIndex);
+            var rightValue = rotatedSortedList.get(rightIndex);
+            var onLeftSide = leftValue < rightValue;
+            var onRightSide = rightValue < leftValue;
+            var diff = (rightIndex - leftIndex) / 2;
+            if(onLeftSide) {
+                rightIndex = rightIndex - diff;
+            } else if(onRightSide) {
+                leftIndex = leftIndex + diff;
+            } else {
+                break;
+            }
+         }
+        return rotatedSortedList.get(leftIndex);
     }
 }
