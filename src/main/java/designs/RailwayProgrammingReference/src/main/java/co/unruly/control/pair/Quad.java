088cced56cca9df6f81fd85e51a033d42aefaf6c
Albert Snow <ajsnow2012@gmail.com>
2023-12-14 00:44:04 -0600

    documentation of Quad.java

--- src/main/java/designs/RailwayProgrammingReference/src/main/java/co/unruly/control/pair/Quad.java
+++ src/main/java/designs/RailwayProgrammingReference/src/main/java/co/unruly/control/pair/Quad.java
@@ -36,15 +36,36 @@ public record Quad<A, B, C, D>(A first, B second, C third, D fourth) {
         T apply(A a, B b, C c, D d);
     }
 
+    /**
+     * @param first .
+     * @param second .
+     * @param third .
+     * @param fourth .
+     * @param <A> .
+     * @param <B> .
+     * @param <C> .
+     * @param <D> .
+     * @return new quad of first-fourth params
+     */
     @Contract("_, _, _, _ -> new")
     public static <A, B, C, D> @NotNull Quad<A, B, C, D> of(A first, B second, C third, D fourth) {
         return new Quad<>(first, second, third, fourth);
     }
 
+    /**
+     * @param function quad function to use
+     * @param <T> return type
+     * @return T given this
+     */
     public <T> T then(@NotNull Function<Quad<A, B, C, D>, T> function) {
         return function.apply(this);
     }
 
+    /**
+     * @param function quad function to use
+     * @param <T> return type
+     * @return T
+     */
     public <T> T then(@NotNull QuadFunction<A, B, C, D, T> function) {
         return function.apply(first, second, third, fourth);
     }
