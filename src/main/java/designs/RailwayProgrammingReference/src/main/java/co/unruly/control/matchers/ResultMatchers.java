05d1a6fa3ca8bf9a77427cfbbabe69119588724b
Albert Snow <ajsnow2012@gmail.com>
2023-12-14 00:40:11 -0600

    documentation to ResultMatchers.java

--- src/main/java/designs/RailwayProgrammingReference/src/main/java/co/unruly/control/matchers/ResultMatchers.java
+++ src/main/java/designs/RailwayProgrammingReference/src/main/java/co/unruly/control/matchers/ResultMatchers.java
@@ -15,33 +15,53 @@ public class ResultMatchers {
 
     /**
      * Matches if the received value is a Success containing the specified value
+     * @param expectedValue success value to test against
+     * @param <S> success
+     * @param <F> failure
+     * @return matcher testing if input is a success containing expected value
      */
     @Contract("_ -> new")
-    public static <S, F> @NotNull Matcher<Result<S, F>> isSuccessOf(S expectedValue) {
+    public static <S, F> @NotNull Matcher<Result<S, F>>
+    isSuccessOf(S expectedValue) {
         return isSuccessThat(equalTo(expectedValue));
     }
 
     /**
      * Matches if the received value is a Success matching the specified value
+     * @param expectedSuccess success to test against
+     * @param <S> success
+     * @param <F> failure
+     * @return matcher testing if input matches success value
      */
     @Contract("_ -> new")
-    public static <S, F> @NotNull Matcher<Result<S, F>> isSuccessThat(Matcher<S> expectedSuccess) {
+    public static <S, F> @NotNull Matcher<Result<S, F>>
+    isSuccessThat(Matcher<S> expectedSuccess) {
         return new SuccessMatcher<>(expectedSuccess);
     }
 
     /**
      * Matches if the received value is a Failure containing the specified value
+     * @param expectedValue value to test failure against
+     * @param <S> success
+     * @param <F> failure
+     * @return matcher that matches input against failure value
      */
     @Contract("_ -> new")
-    public static <S, F> @NotNull Matcher<Result<S, F>> isFailureOf(F expectedValue) {
+    public static <S, F> @NotNull Matcher<Result<S, F>>
+    isFailureOf(F expectedValue) {
         return isFailureThat(equalTo(expectedValue));
     }
 
     /**
      * Matches if the received value is a Failure matching the specified value
+     * @param expectedFailure failure to test against
+     * @param <S> success
+     * @param <F> failure
+     * @return a matcher for the expected failure
      */
     @Contract("_ -> new")
-    public static <S, F> @NotNull Matcher<Result<S, F>> isFailureThat(Matcher<F> expectedFailure) {
+    public static <S, F> @NotNull Matcher<Result<S, F>>
+    isFailureThat(Matcher<F> expectedFailure) {
         return new FailureMatcher<>(expectedFailure);
     }
 
