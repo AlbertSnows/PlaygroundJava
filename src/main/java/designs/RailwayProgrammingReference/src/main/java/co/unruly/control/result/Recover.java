c022b8cf5babcec91b90de3e2fd66f3e884acbcd
Albert Snow <ajsnow2012@gmail.com>
2023-12-14 00:56:51 -0600

    documentation of Recover.java

--- src/main/java/designs/RailwayProgrammingReference/src/main/java/co/unruly/control/result/Recover.java
+++ src/main/java/designs/RailwayProgrammingReference/src/main/java/co/unruly/control/result/Recover.java
@@ -17,9 +17,15 @@ public interface Recover {
     /**
      * Returns a function which takes an Optional value, and returns a failure of the
      * wrapped value if it was present, otherwise returns a success using the provided Supplier
+     * @param onEmpty supplier function
+     * @param <S> success type
+     * @param <F> fail type
+     * @return dF(x -> y) where x is an optional such that if x is a
+     * failure it gets wrapped, otherwise the supplier is called
      */
     @Contract(pure = true)
-    static <S, F> @NotNull Function<Optional<F>, Result<S, F>> whenAbsent(Supplier<S> onEmpty) {
+    static <S, F> @NotNull Function<Optional<F>, Result<S, F>>
+    whenAbsent(Supplier<S> onEmpty) {
         return maybe -> maybe.map(Result::<S, F>failure).orElseGet(() -> Result.success(onEmpty.get()));
     }
 
@@ -27,8 +33,16 @@ public interface Recover {
      * Takes a class and a mapping function and returns a function which takes a value and, if it's of the
      * provided class, applies the mapping function to it and returns it as a Success, otherwise returning
      * the input value as a Failure.
+     * @param targetClass to map to
+     * @param mapper maps from TF to S
+     * @param <S> success type
+     * @param <F> broad fail type
+     * @param <TF> narrow fail type
+     * @return dF(x -> y) where if type of x is TF returns success of mapper(x)
+     * or failure otherwise
      */
-    static <S, F, TF extends F> @NotNull Function<F, Result<S, F>> ifType(Class<TF> targetClass, Function<TF, S> mapper) {
+    static <S, F, TF extends F> @NotNull Function<F, Result<S, F>>
+    ifType(Class<TF> targetClass, Function<TF, S> mapper) {
         return Introducers.<F, TF>castTo(targetClass).andThen(Transformers.onSuccess(mapper));
     }
 
@@ -36,9 +50,16 @@ public interface Recover {
      * Takes a predicate and a mapping function and returns a function which takes a value and, if it satisfies
      * the predicate, applies the mapping function to it and returns it as a Success, otherwise returning
      * the input value as a Failure.
+     * @param test predicate function
+     * @param mapper mapping function
+     * @param <S> success type
+     * @param <F> fail type
+     * @return dF(x -> y) where if x satisfies test, return mapper(x) as a success or
+     * failure otherwise
      */
     @Contract(pure = true)
-    static <S, F> @NotNull Function<F, Result<S, F>> ifIs(Predicate<F> test, Function<F, S> mapper) {
+    static <S, F> @NotNull Function<F, Result<S, F>>
+    ifIs(Predicate<F> test, Function<F, S> mapper) {
         return input -> test.test(input)
             ? Result.success(mapper.apply(input))
             : Result.failure(input);
@@ -48,8 +69,15 @@ public interface Recover {
      * Takes a predicate and a mapping function and returns a function which takes a value and, if it doesn't
      * satisfy the predicate, applies the mapping function to it and returns it as a Success, otherwise returning
      * the input value as a Failure.
+     * @param test testing function
+     * @param mapper mapping function
+     * @param <S> success type
+     * @param <F> fail type
+     * @return dF(x -> y) where if x doesn't satisfy test, x is passed to mapper and put into a success
+     * and is otherwise a failure
      */
-    static <S, F> @NotNull Function<F, Result<S, F>> ifNot(@NotNull Predicate<F> test, Function<F, S> mapper) {
+    static <S, F> @NotNull Function<F, Result<S, F>>
+    ifNot(@NotNull Predicate<F> test, Function<F, S> mapper) {
         return ifIs(test.negate(), mapper);
     }
 
@@ -57,9 +85,15 @@ public interface Recover {
      * Takes a value and a mapping function and returns a function which takes a value and, if it is equal to
      * the provided value, applies the mapping function to it and returns it as a Success, otherwise returning
      * the input value as a Failure.
+     * @param expectedValue value to check against input
+     * @param mapper mapping function for success case
+     * @param <S> success type
+     * @param <F> fail type
+     * @return dF(x -> y) where y is a result whose success applies the mapper to x
      */
     @Contract(pure = true)
-    static <S, F> @NotNull Function<F, Result<S, F>> ifEquals(F expectedValue, Function<F, S> mapper) {
+    static <S, F> @NotNull Function<F, Result<S, F>>
+    ifEquals(F expectedValue, Function<F, S> mapper) {
         return input -> expectedValue.equals(input)
             ? Result.success(mapper.apply(input))
             : Result.failure(input);
@@ -68,9 +102,14 @@ public interface Recover {
     /**
      * Matches the value if the provided function yields an Optional whose value is
      * present, returning the value in that Optional.
+     * @param successProvider function providing an optional success
+     * @param <S> success type
+     * @param <F> fail type
+     * @return function that, given an input, pipes it through success provider into a result
      */
     @Contract(pure = true)
-    static <S, F> @NotNull Function<F, Result<S, F>> ifPresent(Function<F, Optional<S>> successProvider) {
+    static <S, F> @NotNull Function<F, Result<S, F>>
+    ifPresent(Function<F, Optional<S>> successProvider) {
         return value -> successProvider
                 .apply(value)
                 .map(Result::<S, F>success)
