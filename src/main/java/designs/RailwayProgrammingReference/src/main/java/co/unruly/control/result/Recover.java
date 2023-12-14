a4da5a50b14b4108011f350f16a04a4c737f56aa
Albert Snow <ajsnow2012@gmail.com>
2023-12-13 23:53:10 -0600

    contracts + cleanup for Recover.java

--- src/main/java/designs/RailwayProgrammingReference/src/main/java/co/unruly/control/result/Recover.java
+++ src/main/java/designs/RailwayProgrammingReference/src/main/java/co/unruly/control/result/Recover.java
@@ -1,5 +1,8 @@
 package co.unruly.control.result;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Predicate;
@@ -8,13 +11,15 @@ import java.util.function.Supplier;
 /**
  * A collection of functions to (conditionally) recover a failure into a success.
  */
+@SuppressWarnings("unused")
 public interface Recover {
 
     /**
      * Returns a function which takes an Optional value, and returns a failure of the
      * wrapped value if it was present, otherwise returns a success using the provided Supplier
      */
-    static <S, F> Function<Optional<F>, Result<S, F>> whenAbsent(Supplier<S> onEmpty) {
+    @Contract(pure = true)
+    static <S, F> @NotNull Function<Optional<F>, Result<S, F>> whenAbsent(Supplier<S> onEmpty) {
         return maybe -> maybe.map(Result::<S, F>failure).orElseGet(() -> Result.success(onEmpty.get()));
     }
 
@@ -23,7 +28,7 @@ public interface Recover {
      * provided class, applies the mapping function to it and returns it as a Success, otherwise returning
      * the input value as a Failure.
      */
-    static <S, F, TF extends F> Function<F, Result<S, F>> ifType(Class<TF> targetClass, Function<TF, S> mapper) {
+    static <S, F, TF extends F> @NotNull Function<F, Result<S, F>> ifType(Class<TF> targetClass, Function<TF, S> mapper) {
         return Introducers.<F, TF>castTo(targetClass).andThen(Transformers.onSuccess(mapper));
     }
 
@@ -32,7 +37,8 @@ public interface Recover {
      * the predicate, applies the mapping function to it and returns it as a Success, otherwise returning
      * the input value as a Failure.
      */
-    static <S, F> Function<F, Result<S, F>> ifIs(Predicate<F> test, Function<F, S> mapper) {
+    @Contract(pure = true)
+    static <S, F> @NotNull Function<F, Result<S, F>> ifIs(Predicate<F> test, Function<F, S> mapper) {
         return input -> test.test(input)
             ? Result.success(mapper.apply(input))
             : Result.failure(input);
@@ -43,7 +49,7 @@ public interface Recover {
      * satisfy the predicate, applies the mapping function to it and returns it as a Success, otherwise returning
      * the input value as a Failure.
      */
-    static <S, F> Function<F, Result<S, F>> ifNot(Predicate<F> test, Function<F, S> mapper) {
+    static <S, F> @NotNull Function<F, Result<S, F>> ifNot(@NotNull Predicate<F> test, Function<F, S> mapper) {
         return ifIs(test.negate(), mapper);
     }
 
@@ -52,7 +58,8 @@ public interface Recover {
      * the provided value, applies the mapping function to it and returns it as a Success, otherwise returning
      * the input value as a Failure.
      */
-    static <S, F> Function<F, Result<S, F>> ifEquals(F expectedValue, Function<F, S> mapper) {
+    @Contract(pure = true)
+    static <S, F> @NotNull Function<F, Result<S, F>> ifEquals(F expectedValue, Function<F, S> mapper) {
         return input -> expectedValue.equals(input)
             ? Result.success(mapper.apply(input))
             : Result.failure(input);
@@ -62,7 +69,8 @@ public interface Recover {
      * Matches the value if the provided function yields an Optional whose value is
      * present, returning the value in that Optional.
      */
-    static <S, F> Function<F, Result<S, F>> ifPresent(Function<F, Optional<S>> successProvider) {
+    @Contract(pure = true)
+    static <S, F> @NotNull Function<F, Result<S, F>> ifPresent(Function<F, Optional<S>> successProvider) {
         return value -> successProvider
                 .apply(value)
                 .map(Result::<S, F>success)
