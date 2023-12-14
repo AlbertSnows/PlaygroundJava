d997c9c8bdb5ab0438e43d59f6feb175a6d6dae6
Albert Snow <ajsnow2012@gmail.com>
2023-12-14 13:38:33 -0600

    documenting ThrowingLambdas.java

--- src/main/java/designs/RailwayProgrammingReference/src/main/java/co/unruly/control/ThrowingLambdas.java
+++ src/main/java/designs/RailwayProgrammingReference/src/main/java/co/unruly/control/ThrowingLambdas.java
@@ -23,25 +23,27 @@ public interface ThrowingLambdas {
     @FunctionalInterface
     interface ThrowingFunction<I, O, X extends Exception> {
         /**
-         * @param input
-         * @return
-         * @throws X
+         * @param input value
+         * @return O
+         * @throws X exception type
          */
         O apply(I input) throws X;
 
         /**
-         * @param nextFunction
-         * @param <T>
-         * @return
+         * @param nextFunction to use in pipe chain
+         * @param <T> output type
+         * @return dF(x -> y) where x is piped into outer throwing function
+         * and then passed to the next function and y is T or it throws
          */
         default <T> ThrowingFunction<I, T, X> andThen(Function<O, T> nextFunction) {
             return x -> nextFunction.apply(apply(x));
         }
 
         /**
-         * @param nextFunction
-         * @param <T>
-         * @return
+         * @param nextFunction to use in pipe chain
+         * @param <T> input type
+         * @return dF(x -> y) where x is piped into the next function
+         * and y is O or it throws
          */
         default <T> ThrowingFunction<T, O, X> compose(Function<T, I> nextFunction) {
             return x -> apply(nextFunction.apply(x));
@@ -50,11 +52,12 @@ public interface ThrowingLambdas {
         /**
          * Converts the provided function into a regular Function, where any thrown exceptions are
          * wrapped in a RuntimeException.
-         * @param f
-         * @param <I>
-         * @param <O>
-         * @param <X>
-         * @return
+         * @param f I/O function that may throw X
+         * @param <I> input type
+         * @param <O> output type
+         * @param <X> exception type
+         * @return dF(x -> y) where f is passed x and wrapped in a try-catch that
+         * throws a runtime
          */
         @Contract(pure = true)
         static <I, O, X extends Exception> @NotNull Function<I, O>
@@ -76,18 +79,19 @@ public interface ThrowingLambdas {
     @FunctionalInterface
     interface ThrowingConsumer<T, X extends Exception> {
         /**
-         * @param item
-         * @throws X
+         * @param item value
+         * @throws X exception type
          */
         void accept(T item) throws X;
 
         /**
          * Converts the provided consumer into a regular Consumer, where any thrown exceptions are
          * wrapped in a RuntimeException.
-         * @param p
-         * @param <T>
-         * @param <X>
-         * @return
+         * @param p consumer that throws
+         * @param <T> input type
+         * @param <X> exception type
+         * @return dF(x -> y) where x is passed to p and p
+         * is wrapped in a try catch that throws a runtime exception
          */
         @Contract(pure = true)
         static <T, X extends Exception> @NotNull Consumer<T>
@@ -104,21 +108,32 @@ public interface ThrowingLambdas {
 
     /**
      * A BiFunction which may throw a checked exception
+     * @param <A> left type
+     * @param <B> right type
+     * @param <R> result type
+     * @param <X> exception type
      */
     @SuppressWarnings("unused")
     @FunctionalInterface
     interface ThrowingBiFunction<A, B, R, X extends Exception> {
+        /**
+         * @param first .
+         * @param second .
+         * @return R
+         * @throws X exception type
+         */
         R apply(A first, B second) throws X;
 
         /**
          * Converts the provided bifunction into a regular BiFunction, where any thrown exceptions
          * are wrapped in a RuntimeException
-         * @param f
-         * @param <A>
-         * @param <B>
-         * @param <R>
-         * @param <X>
-         * @return
+         * @param f bifunction that may throw
+         * @param <A> left type
+         * @param <B> right type
+         * @param <R> result type
+         * @param <X> exception type
+         * @return dF((a, b) -> y) where a and b are passed to f
+         * and f is wrapped in a try-catch that throws a runtime exception
          */
         @Contract(pure = true)
         static <A, B, R, X extends Exception> @NotNull BiFunction<A, B, R>
@@ -135,25 +150,25 @@ public interface ThrowingLambdas {
 
     /**
      * A Predicate which may throw a checked exception
-     * @param <T>
-     * @param <X>
+     * @param <T> input type
+     * @param <X> exception type
      */
     @FunctionalInterface
     interface ThrowingPredicate<T, X extends Exception> {
         /**
-         * @param item
-         * @return
-         * @throws X
+         * @param item to test
+         * @return boolean result of test
+         * @throws X throw type
          */
         boolean test(T item) throws X;
 
         /**
          * Converts the provided predicate into a regular Predicate, where any thrown exceptions
          * are wrapped in a RuntimeException
-         * @param p
-         * @param <T>
-         * @param <X>
-         * @return
+         * @param p predicate that may throw
+         * @param <T> input type
+         * @param <X> output type
+         * @return dF(x -> y) where p is wrapped in a try catch that throws a runtime
          */
         @Contract(pure = true)
         static <T, X extends Exception> @NotNull Predicate<T>
@@ -169,10 +184,12 @@ public interface ThrowingLambdas {
     }
 
     /**
-     * @param consumer
-     * @param <T>
-     * @param <X>
-     * @return
+     * @param consumer that may throw
+     * @param <T> input type
+     * @param <X> exception type
+     * @return dF(x -> y) where x is passed to the consumer
+     * and the consumer may or may not throw
+     * and y is a boolean that represents if the consumer failed
      */
     @Contract(pure = true)
     static <T, X extends Exception> @NotNull Predicate<T>
@@ -188,10 +205,13 @@ public interface ThrowingLambdas {
     }
 
     /**
-     * @param consumer
-     * @param <T>
-     * @param <X>
-     * @return
+     * @param consumer which may throw
+     * @param <T> input type
+     * @param <X> exception type
+     * @return dF(x -> y) where the consumer may throw
+     * and is thus wrapped in a try-catch that returns
+     * true/false based on whether or not the consumer
+     * threw
      */
     @Contract(pure = true)
     static <T, X extends Exception> @NotNull Predicate<T>
