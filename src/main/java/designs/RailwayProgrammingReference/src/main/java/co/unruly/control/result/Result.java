9a8bfa719c7bd245335628ae5842a421cf33f802
Albert Snow <ajsnow2012@gmail.com>
2023-12-14 00:14:23 -0600

    adding back the failure/success functions. we did, in fact, need them

--- src/main/java/designs/RailwayProgrammingReference/src/main/java/co/unruly/control/result/Result.java
+++ src/main/java/designs/RailwayProgrammingReference/src/main/java/co/unruly/control/result/Result.java
@@ -20,6 +20,7 @@ import java.util.function.Function;
  * @param <S> The type of success
  * @param <F> The type of failure
  */
+@SuppressWarnings("unused")
 public abstract class Result<S, F> implements Serializable {
 
     private Result() {
@@ -37,19 +38,18 @@ public abstract class Result<S, F> implements Serializable {
         return new Success<>(value);
     }
 
-    // is this needed?
-//    /**
-//     * Creates a new Success, taking the failure type for contexts where it can't be inferred.
-//     * @param value to wrap
-//     * @param failureType class type
-//     * @param <S> success type
-//     * @param <F> failure type
-//     * @return S wrapped in success
-//     */
-//    @Contract("_, _ -> new")
-//    public static <S, F> @NotNull Result<S, F> success(S value, Class<F> failureType) {
-//        return new Success<>(value);
-//    }
+    /**
+     * Creates a new Success, taking the failure type for contexts where it can't be inferred.
+     * @param value to wrap
+     * @param failureType class type
+     * @param <S> success type
+     * @param <F> failure type
+     * @return S wrapped in success
+     */
+    @Contract("_, _ -> new")
+    public static <S, F> @NotNull Result<S, F> success(S value, Class<F> failureType) {
+        return new Success<>(value);
+    }
 
     /**
      * Creates a new Failure
@@ -63,19 +63,18 @@ public abstract class Result<S, F> implements Serializable {
         return new Failure<>(error);
     }
 
-    // is this needed?
-//    /**
-//     * Creates a new Failure, taking the success type for contexts where it can't be inferred.
-//     * @param error to wrap
-//     * @param successType for context
-//     * @param <S> success type
-//     * @param <F> fail type
-//     * @return F wrapped in failure
-//     */
-//    @Contract("_, _ -> new")
-//    public static <S, F> @NotNull Result<S, F> failure(F error, Class<S> successType) {
-//        return new Failure<>(error);
-//    }
+    /**
+     * Creates a new Failure, taking the success type for contexts where it can't be inferred.
+     * @param error to wrap
+     * @param successType for context
+     * @param <S> success type
+     * @param <F> fail type
+     * @return F wrapped in failure
+     */
+    @Contract("_, _ -> new")
+    public static <S, F> @NotNull Result<S, F> failure(F error, Class<S> successType) {
+        return new Failure<>(error);
+    }
 
     /**
      * Takes two functions, the first of which is executed in the case that this
