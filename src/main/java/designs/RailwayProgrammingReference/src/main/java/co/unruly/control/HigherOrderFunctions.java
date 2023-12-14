cc455bb5c7900dc3a62eaacdea5849a02980faec
Albert Snow <ajsnow2012@gmail.com>
2023-12-13 22:32:29 -0600

    documentation for HOF

--- src/main/java/designs/RailwayProgrammingReference/src/main/java/co/unruly/control/HigherOrderFunctions.java
+++ src/main/java/designs/RailwayProgrammingReference/src/main/java/co/unruly/control/HigherOrderFunctions.java
@@ -24,6 +24,11 @@ public interface HigherOrderFunctions {
 
     /**
      * Takes a BiFunction, and reverses the order of the arguments
+     * @param f function to use
+     * @param <A> left type
+     * @param <B> right type
+     * @param <R> result type
+     * @return dF where f(a, b) now becomes f(b, a)
      */
     @Contract(pure = true)
     static <A, B, R> @NotNull BiFunction<B, A, R> flip(BiFunction<A, B, R> f) {
@@ -33,6 +38,9 @@ public interface HigherOrderFunctions {
     /**
      * Takes a list of functions (which take and return the same type) and composes
      * them into a single function, applying the provided functions in order
+     * @param functions an arbitrary number of T -> T functions
+     * @param <T> function type
+     * @return a single function representing the composition of each composite function
      */
     @SafeVarargs
     static <T> Function<T, T> compose(Function<T, T>... functions) {
@@ -42,6 +50,9 @@ public interface HigherOrderFunctions {
     /**
      * Takes a Stream of functions (which take and return the same type) and composes
      * them into a single function, applying the provided functions in order
+     * @param functions functions to compose
+     * @param <T> input type
+     * @return a single function that is the combination of each function
      */
     static <T> Function<T, T> compose(@NotNull Stream<Function<T, T>> functions) {
         return functions.reduce(identity(), Function::andThen);
@@ -50,6 +61,9 @@ public interface HigherOrderFunctions {
     /**
      * Takes a list of predicates and composes them into a single predicate, which
      * passes when all passed-in predicates pass
+     * @param functions functions to compose
+     * @param <T> input type
+     * @return predicate function that is the composition of all passed in predicates
      */
     @SafeVarargs
     static <T> Predicate<T> compose(Predicate<T>... functions) {
@@ -58,6 +72,9 @@ public interface HigherOrderFunctions {
 
     /**
      * Turns a Consumer into a Function which applies the consumer and returns the input
+     * @param action consumer to act
+     * @param <T> input type
+     * @return dF (x -> x) where x is passed to action to execute
      */
     @Contract(pure = true)
     static <T> @NotNull Function<T, T> peek(Consumer<T> action) {
@@ -97,8 +114,16 @@ public interface HigherOrderFunctions {
      * <p>
      * Sourced from
      * <a href="https://stackoverflow.com/questions/17640754/zipping-streams-using-jdk8-with-lambda-java-util-stream-streams-zip">here</a>
+     * @param a left stream
+     * @param b right stream
+     * @param zipper zipping function
+     * @param <A> left type
+     * @param <B> right type
+     * @param <C> result type
+     * @return a stream representing the merging of streams a and b
      */
-    static <A , B, C> @NotNull Stream<C> zip(Stream<A> a, Stream<B> b, BiFunction<A, B, C> zipper) {
+    static <A , B, C> @NotNull Stream<C>
+    zip(Stream<A> a, Stream<B> b, BiFunction<A, B, C> zipper) {
         Objects.requireNonNull(zipper);
         Spliterator<? extends A> aSpliterator = Objects.requireNonNull(a).spliterator();
         Spliterator<? extends B> bSpliterator = Objects.requireNonNull(b).spliterator();
@@ -133,13 +158,23 @@ public interface HigherOrderFunctions {
 
     /**
      * Takes two lists, and returns a list of pairs forming the Cartesian product of those lists.
+     * @param as left list
+     * @param bs right list
+     * @param <A> left type
+     * @param <B> right type
+     * @return list of pairs in the form L[P(A, B)...]
      */
-    static <A, B> List<Pair<A, B>> pairs(List<A> as, List<B> bs) {
+    static <A, B> List<Pair<A, B>>
+    pairs(@NotNull List<A> as, List<B> bs) {
         return as.stream().flatMap(a -> bs.stream().map(b -> Pair.of(a, b))).collect(toList());
     }
 
     /**
      * Takes a value, and returns that same value, upcast to a suitable type. Inference is our friend here.
+     * @param fv value to upcast
+     * @param <R> type to upcast to
+     * @param <T> value type
+     * @return fv upcasted from T to R
      */
     static <R, T extends R> R upcast(T fv) {
         return fv;
