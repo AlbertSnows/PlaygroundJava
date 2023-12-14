cc147c4a85db6939770291ff19033727e0bec694
Albert Snow <ajsnow2012@gmail.com>
2023-12-13 22:55:31 -0600

    documenting MonadicAliases.java

--- src/main/java/designs/RailwayProgrammingReference/src/main/java/co/unruly/control/result/MonadicAliases.java
+++ src/main/java/designs/RailwayProgrammingReference/src/main/java/co/unruly/control/result/MonadicAliases.java
@@ -1,5 +1,8 @@
 package co.unruly.control.result;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.function.Function;
 
 import static co.unruly.control.result.Introducers.success;
@@ -9,20 +12,32 @@ import static co.unruly.control.result.Transformers.*;
  * Aliases for standard functions on Results which use names more familiar
  * to users of Haskell
  */
+@SuppressWarnings("unused")
 public interface MonadicAliases {
 
     /**
      * Returns a function which converts a regular value into a Result (as a Success)
+     * @param <S>
+     * @param <F>
+     * @return
      */
-    static <S, F> Function<S, Result<S, F>> pure() {
+    @Contract(pure = true)
+    static <S, F> @NotNull Function<S, Result<S, F>>
+    pure() {
         return success();
     }
 
     /**
      * Returns a function which, when applied to a Result, applies the provided function to
      * the wrapped value if it's a Success, otherwise perpetuates the existing failure
+     * @param f
+     * @param <S>
+     * @param <S1>
+     * @param <F>
+     * @return
      */
-    static <S, S1, F> Function<Result<S, F>, Result<S1, F>> map(Function<S, S1> f) {
+    static <S, S1, F> @NotNull Function<Result<S, F>, Result<S1, F>>
+    map(Function<S, S1> f) {
         return onSuccess(f);
     }
 
@@ -30,29 +45,50 @@ public interface MonadicAliases {
      * Returns a function which, when applied to a Result, applies the provided function to
      * the wrapped value, returning that Result, if it's a Success. This can turn a Success into a
      * Failure.
-     *
+     * <p>
      * If the result was already a failure, it perpetuates the existing failure.
+     * @param f
+     * @param <S>
+     * @param <S1>
+     * @param <F>
+     * @return
      */
-    static <S, S1, F> Function<Result<S, F>, Result<S1, F>> flatMap(Function<S, Result<S1, F>> f) {
+    @Contract(pure = true)
+    static <S, S1, F> @NotNull Function<Result<S, F>, Result<S1, F>>
+    flatMap(Function<S, Result<S1, F>> f) {
         return attempt(f);
     }
+
 
     /**
      * Returns a function which, when applied to a Result, applies the provided function to
      * the wrapped value, returning that Result, if it's a Success. This can turn a Success into a
      * Failure.
-     *
+     * <p>
      * If the result was already a failure, it perpetuates the existing failure.
+     * @param f
+     * @param <S>
+     * @param <S1>
+     * @param <F>
+     * @return
      */
-    static <S, S1, F> Function<Result<S, F>, Result<S1, F>> bind(Function<S, Result<S1, F>> f) {
+    @Contract(pure = true)
+    static <S, S1, F> @NotNull Function<Result<S, F>, Result<S1, F>>
+    bind(Function<S, Result<S1, F>> f) {
         return attempt(f);
     }
 
     /**
      * Returns a function which, when applied to a Result, applies the provided function to
      * the wrapped value if it's a failure, otherwise perpetuates the existing success
+     * @param f
+     * @param <S>
+     * @param <F>
+     * @param <F1>
+     * @return
      */
-    static <S, F, F1> Function<Result<S, F>, Result<S, F1>> mapFailure(Function<F, F1> f) {
+    static <S, F, F1> @NotNull Function<Result<S, F>, Result<S, F1>>
+    mapFailure(Function<F, F1> f) {
         return onFailure(f);
     }
 
@@ -60,10 +96,17 @@ public interface MonadicAliases {
      * Returns a function which, when applied to a Result, applies the provided function to
      * the wrapped value, returning that Result, if it's a Failure. This can turn a Failure into a
      * Success.
-     *
+     * <p>
      * If the result was already a success, it perpetuates the existing success.
+     * @param f
+     * @param <S>
+     * @param <F>
+     * @param <F1>
+     * @return
      */
-    static <S, F, F1> Function<Result<S, F>, Result<S, F1>> flatMapFailure(Function<F, Result<S, F1>> f) {
+    @Contract(pure = true)
+    static <S, F, F1> @NotNull Function<Result<S, F>, Result<S, F1>>
+    flatMapFailure(Function<F, Result<S, F1>> f) {
         return recover(f);
     }
 
@@ -72,10 +115,17 @@ public interface MonadicAliases {
      * Returns a function which, when applied to a Result, applies the provided function to
      * the wrapped value, returning that Result, if it's a Failure. This can turn a Failure into a
      * Success.
-     *
+     * <p>
      * If the result was already a success, it perpetuates the existing success.
+     * @param f
+     * @param <S>
+     * @param <F>
+     * @param <F1>
+     * @return
      */
-    static <S, F, F1> Function<Result<S, F>, Result<S, F1>> bindFailure(Function<F, Result<S, F1>> f) {
+    @Contract(pure = true)
+    static <S, F, F1> @NotNull Function<Result<S, F>, Result<S, F1>>
+    bindFailure(Function<F, Result<S, F1>> f) {
         return recover(f);
     }
 }
