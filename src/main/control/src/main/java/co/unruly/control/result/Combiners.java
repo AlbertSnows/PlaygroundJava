711da432f8f6ab0ea8568310b48d629f35c3f0b5
Albert Snow <ajsnow2012@gmail.com>
2023-12-28 18:29:13 -0600

    did i fix it?

--- src/main/control/src/main/java/co/unruly/control/result/Combiners.java
+++ src/main/control/src/main/java/co/unruly/control/result/Combiners.java
@@ -1,46 +1,53 @@
 package co.unruly.control.result;
 
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
 import static co.unruly.control.result.Result.success;
+import static co.unruly.control.result.Transformers.attempt;
 
 /**
  * interface for combining different data structures
  */
 public interface Combiners {
 
+    @Contract(pure = true)
+    static <LS, RS, C> @NotNull Function<LS, Function<RS, C>> curryCombiner(BiFunction<LS, RS, C> combiner) {
+        return leftSuccess -> rightSuccess -> combiner.apply(leftSuccess, rightSuccess);
+    }
+
     /**
      * Combines two Results into a single Result. If both arguments are a Success, then
      * it applies the given function to their values and returns a Success of it.
-     * @param <A> a
-     * @param <B> b
-     * @param <F> f
-     * @param secondArgument result to merge
+     * @param <LS> left success type
+     * @param <RS> right success type
+     * @param <F> failure type
+     * @param leftResult left result to merge
      * @return first failure
      * If either or both arguments are Failures, then this returns the first failure
      * it encountered.
      */
     @org.jetbrains.annotations.NotNull
     @org.jetbrains.annotations.Contract(pure = true)
-    static <A, B, F> Function<Result<A, F>, MergeableResults<A, B, F>>
-    combineWith(Result<B, F> secondArgument) {
+    static <LS, RS, F> Function<Result<RS, F>, MergeableResults<LS, RS, F>>
+    combineWith(Result<LS, F> leftResult) {
         // ugh ugh ugh we need an abstract class because otherwise it can't infer generics properly can i be sick now? ta
-        return result -> new MergeableResults<>() {
+        return rightResult -> new MergeableResults<>() {
             /**
              * @param combiner combining mechanism
              * @param <C>      combiner
              * @return an either
              */
             @Override
-            public <C> Result<C, F> using(BiFunction<A, B, C> combiner) {
-                return result.either(
-                        s1 -> secondArgument.either(
-                                s2 -> success(combiner.apply(s1, s2)),
-                                Result::failure
-                        ),
-                        Result::failure
-                );
+            public <C> Result<C, F> using(BiFunction<LS, RS, C> combiner) {
+                var curriedCombiner = curryCombiner(combiner);
+
+                return leftResult
+                        .then(attempt(s1 -> rightResult
+                                .then(attempt(s2 -> success(curriedCombiner.apply(s1).apply(s2))))));
             }
         };
     }
