9cae4f275a48028f698a8da63f5543b76a7de491
Albert Snow <ajsnow2012@gmail.com>
2024-01-21 16:29:02 -0600

    implemented double linked list

--- src/main/java/playground/examples/doublelinkedlist/Core.java
+++ src/main/java/playground/examples/doublelinkedlist/Core.java
@@ -1,4 +1,16 @@
 package playground.examples.doublelinkedlist;
 
+import java.util.List;
+
 public class Core {
+    public static void main(String[] args) {
+        var example = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        var nodes = DoubleNode.from(example).next;
+        while(nodes.next != null) {
+            System.out.println("before: " + nodes.prev.content);
+            System.out.println("here: " + nodes.content);
+            System.out.println("next: " + nodes.next.content);
+            nodes = nodes.next;
+        }
+    }
 }
--- src/main/java/playground/examples/doublelinkedlist/DoubleNode.java
+++ src/main/java/playground/examples/doublelinkedlist/DoubleNode.java
@@ -1,13 +1,36 @@
 package playground.examples.doublelinkedlist;
 
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.IntConsumer;
+import java.util.stream.IntStream;
 
 public class DoubleNode<C> {
-    final C content;
-    private DoubleNode<C> prev;
-    private DoubleNode<C> next;
+    C content;
+    DoubleNode<C> prev;
+    DoubleNode<C> next;
     DoubleNode(C contents) {
         this.content = contents;
+    }
+
+    public static <C> DoubleNode<C> from(@NotNull List<C> list) {
+        var length = list.size();
+        var nodeList = list.stream()
+                .map(DoubleNode::new).toList();
+        IntConsumer addNeighbors = i -> nodeList.get(i).between()
+                        .apply(nodeList.get(i-1))
+                        .apply(nodeList.get(i+1));
+        IntStream.rangeClosed(1, length - 2).forEach(addNeighbors);
+        nodeList.get(length - 1).aheadOf(nodeList.get(length - 2));
+        nodeList.get(0).priorTo(nodeList.get(1));
+        return nodeList.get(0);
+    }
+    public DoubleNode<C> content(C data) {
+        this.content = data;
+        return this;
     }
     public DoubleNode<C> aheadOf(DoubleNode<C> prior) {
         this.prev = prior;
@@ -20,6 +43,6 @@ public class DoubleNode<C> {
     }
 
     public Function<DoubleNode<C>, Function<DoubleNode<C>, DoubleNode<C>>> between() {
-        return prior -> leader -> this.priorTo(prior).aheadOf(leader);
+        return prior -> leader -> this.priorTo(leader).aheadOf(prior);
     }
 }
